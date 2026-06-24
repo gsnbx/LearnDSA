137b8e032dd5c760cdeab4980a750a0ea1ad915a
gsnbx <swaminathan.bx@gmail.com>
2026-06-24 13:03:11 +0530

    minmax added

--- Arrays/FindMinMaxArray.java
+++ Arrays/FindMinMaxArray.java
@@ -1,5 +1,19 @@
 class FindMinMaxArray{
     public static void main(String args[]){
-        System.out.println("hellomssubishasrijdimissreallyudidumissme");
+        
+        int[] arr = { 10,20,30,5,50,40 };
+        
+        int min=arr[0], max = arr[0];
+        
+        for(int i=0; i<arr.length; i++){
+            
+            if(min > arr[i])
+                min = arr[i];
+            
+            if(max < arr[i])
+                max = arr[i];
+        }
+        System.out.println("min = "+min);
+        System.out.println("max = "+max);
     }
 }
